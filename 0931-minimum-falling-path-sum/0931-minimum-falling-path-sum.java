f87249f54a67b2a0f3f81fa0bdb2e4c02aeebbaa
Sanket Badjate <32664738+sanbad36@users.noreply.github.com>
2024-02-15 01:31:05 +0530

    Time: 9 ms (20.50%), Space: 45.4 MB (33.20%) - LeetHub

---
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -0,0 +1,50 @@
+class Solution {
+    
+    int f(int[][] matrix){
+        
+       
+       int n = matrix.length;
+    int m = matrix[0].length;
+    
+    int dp[][] = new int[n][m];
+    
+    // Initializing first row - base condition
+    for(int j=0; j<m; j++){
+        dp[0][j] = matrix[0][j];
+    }
+    
+    for(int i=1; i<n; i++){
+        for(int j=0;j<m;j++){
+            
+            int up = matrix[i][j] + dp[i-1][j];
+            
+            int leftDiagonal= matrix[i][j];
+            if(j-1>=0) leftDiagonal += dp[i-1][j-1];
+            else leftDiagonal += (int)Math.pow(10,9);
+            
+            int rightDiagonal = matrix[i][j];
+            if(j+1<m) rightDiagonal += dp[i-1][j+1];
+            else rightDiagonal += (int)Math.pow(10,9);
+            
+            dp[i][j] = Math.min(up, Math.min(leftDiagonal,rightDiagonal));
+            
+        }
+    }
+    
+    int mini = Integer.MAX_VALUE;
+    
+    for(int j=0; j<m;j++){
+        mini = Math.min(mini,dp[n-1][j]);
+    }
+    
+    return mini;
+        
+    }
+    
+    public int minFallingPathSum(int[][] matrix) {
+      
+      
+        return f(matrix);
+        
+    }
+}
