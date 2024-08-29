8728439e1291f8de7729e326a01c1b1f797eee3a
Sanket Badjate <32664738+sanbad36@users.noreply.github.com>
2024-08-29 09:48:38 +0530

    Time: 214 ms (5.09%), Space: 45.1 MB (37.69%) - LeetHub

---
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int removeStones(int[][] stones) {
+        int count=0;
+        Set<int[]> visited=new HashSet<>();
+        for(int i=0;i<stones.length;i++){
+           if(!visited.contains(stones[i])){
+                visited.add(stones[i]);
+                removeStonesUtil(visited,stones,i);
+                count++;
+           }         
+        }
+        return stones.length-count;
+    }
+    public void removeStonesUtil(Set<int[]> visited,int stones[][],int idx){
+        visited.add(stones[idx]);
+        for(int i=0;i<stones.length;i++){
+           if(!visited.contains(stones[i])){
+               if(stones[idx][0]==stones[i][0] || stones[idx][1]==stones[i][1]){
+                   removeStonesUtil(visited,stones,i);
+               }   
+           }         
+        }
+    }
+}
