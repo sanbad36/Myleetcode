e6daf2323d890f321ae5a4bb4695c8b4a4633735
Sanket Badjate <32664738+sanbad36@users.noreply.github.com>
2024-09-14 17:01:44 +0530

    Time: 1 ms (27.39%), Space: 41.3 MB (28.69%) - LeetHub

---
+++ 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int countSegments(String s) {
+ 
+        String trimmed = s.trim();
+        if (trimmed.equals("")) {
+            return 0;
+        }
+        return trimmed.split("\\s+").length;
+    }
+}
